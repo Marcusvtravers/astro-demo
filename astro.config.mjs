a224e3d0616751a3c4ffa65b67e08f77af91c4cc
marcusvint <marcusvtravers@gmail.com>
2025-09-15 19:52:59 +0100

    created rss xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://enchanting-horse-16cdbd.netlify.app/"
+});
